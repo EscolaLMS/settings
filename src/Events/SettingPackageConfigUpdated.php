3a7968a70a00ffa10a06cec77f45ea2cf4a379a0
Hubert Krzysztofiak <hubertkrzyszt@gmail.com>
2022-01-24 13:47:42 +0100

    Repair event methods

--- src/Events/SettingPackageConfigUpdated.php
+++ src/Events/SettingPackageConfigUpdated.php
@@ -7,14 +7,21 @@ use Illuminate\Contracts\Auth\Authenticatable;
 class SettingPackageConfigUpdated
 {
     private Authenticatable $user;
+    private array $config;
 
-    public function __construct(Authenticatable $user)
+    public function __construct(Authenticatable $user, array $config)
     {
         $this->user = $user;
+        $this->config = $config;
     }
 
     public function getUser(): Authenticatable
     {
         return $this->user;
+    }
+
+    public function getConfig(): array
+    {
+        return $this->config;
     }
 }
