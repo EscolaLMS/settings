7363eedebd5d32434f5c930b7fec12c8c717b7cc
Maciej Rymarz <59456825+mako321@users.noreply.github.com>
2022-11-18 16:40:19 +0100

    Add translations (#23)

---
+++ resources/lang/en/permissions.php
@@ -0,0 +1,14 @@
+<?php
+
+use EscolaLms\Settings\Enums\SettingsPermissionsEnum;
+
+return [
+    SettingsPermissionsEnum::SETTINGS_MANAGE => 'Manage setting',
+    SettingsPermissionsEnum::SETTINGS_CREATE => 'Create settings',
+    SettingsPermissionsEnum::SETTINGS_DELETE => 'Delete settings',
+    SettingsPermissionsEnum::SETTINGS_UPDATE => 'Update settings',
+    SettingsPermissionsEnum::SETTINGS_READ => 'Read settings',
+    SettingsPermissionsEnum::SETTINGS_LIST => 'Settings list',
+    SettingsPermissionsEnum::CONFIG_LIST => 'Config list',
+    SettingsPermissionsEnum::CONFIG_UPDATE => 'Update config',
+];
