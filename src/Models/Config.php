4a867b73b611591fe730e687eb3a4da2aced56d8
Witold Wiśniewski <witold.michal.wisniewski@gmail.com>
2024-07-19 13:29:46 +0200

    fix

--- src/Models/Config.php
+++ src/Models/Config.php
@@ -4,6 +4,10 @@ namespace EscolaLms\Settings\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property array<string, mixed> $value
+ */
 class Config extends Model
 {
     public $table = 'config';
